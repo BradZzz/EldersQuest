c6626de5028cb1e8cc5852c7bb356c2b8b2fdcba
BradZzz <mauvemoonman@gmail.com>
2019-01-26 00:16:15 -0800

    for save

--- Assets/Scripts/HighlightShift.cs
+++ Assets/Scripts/HighlightShift.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 
 public class HighlightShift : MonoBehaviour
 {
-  private void Awake()
+  private void Start()
   {
-    iTween.MoveTo(gameObject, iTween.Hash("y", .29f, "islocal", true, "time", 1.2f, "looptype", "pingPong", "easetype", "spring"));
+    //Color clr = Color.white;
+    //clr.a = 0;
+    //GetComponent<SpriteRenderer>().color = clr;
+    //foreach (Material mObj in GetComponent<Renderer>().materials)
+    //{
+    //  mObj.color = new Color(
+    //      mObj.color.r, mObj.color.g,
+    //      mObj.color.b, 0);
+    //}
+    //iTween.MoveTo(gameObject, iTween.Hash("y", .29f, "islocal", true, "time", 1.2f, "looptype", "pingPong", "easetype", "spring"));
+    //GetComponent<FadeMaterials>().FadeIn();
+    //StartCoroutine(FadeIn());
   }
+
+  void OnEnable()
+  {
+    foreach (Material mObj in GetComponent<Renderer>().materials)
+    {
+      mObj.color = new Color(
+          mObj.color.r, mObj.color.g,
+          mObj.color.b, 0);
+    }
+    iTween.MoveTo(gameObject, iTween.Hash("y", .29f, "islocal", true, "time", 1.2f, "looptype", "pingPong", "easetype", "spring"));
+    GetComponent<FadeMaterials>().FadeIn();
+  }
+
+  //IEnumerator FadeIn()
+  //{
+  //  //iTween.FadeTo(gameObject, 1, .5f);
+  //  //yield return new WaitForSeconds(.5f);
+  //  GetComponent<FadeMaterials>().FadeIn();
+  //  yield return new WaitForSeconds(.5f);
+  //  iTween.MoveTo(gameObject, iTween.Hash("y", .29f, "islocal", true, "time", 1.2f, "looptype", "pingPong", "easetype", "spring"));
+  //}
 }

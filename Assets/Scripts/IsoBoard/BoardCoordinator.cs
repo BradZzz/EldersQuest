6c96147dd2399901c6e017d13acfc4fc31c4affa
BradZzz <mauvemoonman@gmail.com>
2019-02-03 07:08:45 -0800

    for save

--- Assets/Scripts/IsoBoard/BoardCoordinator.cs
+++ Assets/Scripts/IsoBoard/BoardCoordinator.cs
@@ -63,6 +63,7 @@ public class BoardCoordinator : MonoBehaviour
           selected = val;
         }
       }
+      PanelController.instance.SwitchCharImages(GetChar(selected).south);
     }
   }
 
---
+++ Assets/Scripts/IsoBoard/PanelController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour
+{
+  public static PanelController instance;
+  public GameObject ImagePanel;
+
+  private void Awake()
+  {
+    instance = this;
+  }
+
+  public void SwitchCharImages(Sprite sprite)
+  {
+    ImagePanel.GetComponent<Image>().sprite = sprite;
+  }
+}

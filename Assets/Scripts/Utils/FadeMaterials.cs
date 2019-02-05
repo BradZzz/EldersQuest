1c419a748f7136af15b4323fc7d219eb8cc5e0f4
BradZzz <mauvemoonman@gmail.com>
2019-02-04 17:13:42 -0800

    for save

--- Assets/Scripts/Utils/FadeMaterials.cs
+++ Assets/Scripts/Utils/FadeMaterials.cs
@@ -20,11 +20,25 @@ class FadeMaterials : MonoBehaviour
   }
   public void setAlpha(float newAlpha)
   {
-    foreach (Material mObj in GetComponent<Renderer>().materials)
+    if (GetComponent<Renderer>() != null)
     {
-      mObj.color = new Color(
+      foreach (Material mObj in GetComponent<Renderer>().materials)
+      {
+        mObj.color = new Color(
           mObj.color.r, mObj.color.g,
           mObj.color.b, newAlpha);
+      }
+    }
+
+    if (GetComponent<CanvasRenderer>() != null)
+    {
+      for (int i = 0; i < GetComponent<CanvasRenderer>().materialCount; i++)
+      {
+        Material mat = GetComponent<CanvasRenderer>().GetMaterial(i);
+        mat.color = new Color(
+          mat.color.r, mat.color.g,
+          mat.color.b, newAlpha);
+      }
     }
   }
 
--- Assets/Scripts/Utils/HighlightShift.cs
+++ Assets/Scripts/Utils/HighlightShift.cs
@@ -8,12 +8,27 @@ public class HighlightShift : MonoBehaviour
 
   void OnEnable()
   {
-    foreach (Material mObj in GetComponent<Renderer>().materials)
+    if (GetComponent<Renderer>() != null)
     {
-      mObj.color = new Color(
-          mObj.color.r, mObj.color.g,
-          mObj.color.b, 0);
+      foreach (Material mObj in GetComponent<Renderer>().materials)
+      {
+        mObj.color = new Color(
+            mObj.color.r, mObj.color.g,
+            mObj.color.b, 0);
+      }
     }
+
+    if (GetComponent<CanvasRenderer>() != null)
+    {
+      for (int i = 0; i < GetComponent<CanvasRenderer>().materialCount; i++)
+      {
+        Material mat = GetComponent<CanvasRenderer>().GetMaterial(i);
+        mat.color = new Color(
+            mat.color.r, mat.color.g,
+            mat.color.b, 0);
+      }
+    }
+
     iTween.MoveTo(gameObject, iTween.Hash("y", y, "islocal", true, "time", 1.2f, "looptype", "pingPong", "easetype", "spring"));
     GetComponent<FadeMaterials>().FadeIn();
   }

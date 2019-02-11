ba021954a8fad96cbea7787706a23c2066722fe8
Alessandro <alessandro.fama@ymail.com>
2019-02-11 05:36:16 +0100

    Char select sfx

--- Assets/Audio/Scripts/FMODPaths.cs
+++ Assets/Audio/Scripts/FMODPaths.cs
@@ -5,6 +5,11 @@
     public static readonly string SelectSaveSound = "event:/UI/select_save";
     public static readonly string SelectWorldSound = "event:/UI/select_world";
 
+    public static readonly string SelectCharMonsterSound = "event:/UI/char_select/select_monster";
+    public static readonly string SelectCharCreatureSound = "event:/UI/char_select/select_creature";
+    public static readonly string SelectCharMageSound = "event:/UI/char_select/select_mage";
+    public static readonly string SelectCharAcceptSound = "event:/UI/char_select/select_accept";
+
 
     #endregion
 

5536ba056f1a96dea794ef8ad819b5998baf7931
BradZzz <mauvemoonman@gmail.com>
2019-03-12 19:09:08 -0700

    fixed forceatk skill

--- Assets/Scripts/Stats/StatsNav.cs
+++ Assets/Scripts/Stats/StatsNav.cs
@@ -8,8 +8,10 @@ using UnityEngine.UI;
 
 public class StatsNav : MonoBehaviour
 {
-    public GameObject hScorePnl, classPnl, skillPnl;
+    public static StatsNav instance;
 
+    public GameObject hScorePnl, classPnl, skillPnl;
+    public GameObject skillRw;
     public GameObject hScoreBtn, classBtn, skillBtn;
 
     public enum Location{
@@ -19,13 +21,13 @@ public class StatsNav : MonoBehaviour
     private Location thisLoc;
 
     void Awake(){
+        instance = this;
         thisLoc = Location.highscores;
         Refresh();
     }
 
     public void Click(int loc){
         thisLoc = (Location)loc;
-
         Refresh();
     }
 
@@ -77,13 +79,37 @@ public class StatsNav : MonoBehaviour
         skillPnl.SetActive(true); 
         skillBtn.GetComponent<Outline>().effectColor = Color.red;
         GameMeta game = BaseSaver.GetGame();
-        string pnlString = "\n";
+        string pnlString = "";
         if (game.skillsSeen.Length > 0) {
+          game.skillsSeen = game.skillsSeen.OrderBy(nm=>nm).ToArray();
           foreach(string skll in game.skillsSeen){
+              GameObject skllCpy = Instantiate(skillRw, skillPnl.transform.GetChild(0).GetChild(0));
+              skllCpy.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = skll;
               Skill tSkill = Skill.ReturnSkillByString((Skill.SkillClasses)Enum.Parse(typeof(Skill.SkillClasses), skll));
-              pnlString += skll + ": " + tSkill.PrintDetails() + "\n";
+              //pnlString = skll + ": " + tSkill.PrintDetails() + "\n";
+              Debug.Log("pnlString: " + skll + ": " + tSkill.PrintDetails() + "\n");
+              //UnityEngine.Events.UnityAction action1 = () => { instance.SetSkillInfoText(pnlString); };
+              skllCpy.GetComponent<Button>().onClick.AddListener(() => { instance.SetSkillInfoText(skll + ": " + tSkill.PrintDetails() + "\n"); });
+              
+
+              //Skill tSkill = Skill.ReturnSkillByString((Skill.SkillClasses)Enum.Parse(typeof(Skill.SkillClasses), skll));
+              //pnlString += skll + ": " + tSkill.PrintDetails() + "\n";
           }
         }
-        skillPnl.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = pnlString;
+        skillPnl.transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().text = pnlString;
+
+
+        //GameObject nwRow = Instantiate(chrRW, chrSelect.transform);
+        //nwRow.GetComponent<TechTreeUnitWrap>().unit = unt;
+        //nwRow.GetComponent<TechTreeUnitWrap>().Refresh();
+        //RefreshMainPanel(nwRow, unt, idx);
+
+        //UnityEngine.Events.UnityAction action1 = () => { instance.CharClicked(unt); };
+        //nwRow.GetComponent<Button>().onClick.AddListener(action1);
+    }
+
+    public void SetSkillInfoText(string msg){
+        Debug.Log("SetSkillInfoText: " + msg);
+        skillPnl.transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().text = msg;
     }
 }
